deb5e029afb27279586dc998600ac5e0033b77ee
Rodrigo Garcia <rgflsc@gmail.com>
2019-06-26 21:51:24 -0300

    commit

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,4 +1,4 @@
 module.exports = {
-  secret: 'marketplace',
+  secret: process.env.APP_SECRET,
   ttl: 86400
 }
--- src/config/mail.js
+++ src/config/mail.js
@@ -1,9 +1,9 @@
 module.exports = {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '1899d454172077',
-    pass: '698ebdd625c2d9'
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS
   }
 }
